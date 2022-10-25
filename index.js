63860a806ec437ad47b7c318383629077d553984
AlyssiaPH <alyssia0207@gmail.com>
2022-10-25 13:53:36 +0200

    remove useless code

--- index.js
+++ index.js
@@ -9,20 +9,6 @@ const port = 3000;
 const express = require('express');
 const app = express();
 
-
-
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
-    //Set the response HTTP header with HTTP status and Content type
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World\n");
-    server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-});
-
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
@@ -32,7 +18,6 @@ app.listen(port);
 console.log('Server started at http://localhost:' + port);
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
-
 
 /*const serialport = new SerialPort({
     path: '/dev/tty.BLTH',
